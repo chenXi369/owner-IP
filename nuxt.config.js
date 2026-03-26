e560d2197545f4b7d2e73fddd111e196ec322613
“lichengcheng” <li_chenchen77@163.com>
2026-03-26 12:23:25 +0800

    ci: 更新部署配置以简化Nuxt设置
    移除nuxt.config.js中冗余的baseURL配置，改为在GitHub Actions中通过环境变量设置NUXT_APP_BASE_URL

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  
   devtools: { enabled: true },
   
   css: ['~/assets/styles/main.scss'],
@@ -12,10 +10,6 @@ export default defineNuxtConfig({
       pathPrefix: false
     }
   ],
-  
-  app: {
-    baseURL: '/owner-IP/'
-  },
   
   nitro: {
     preset: 'github-pages'
